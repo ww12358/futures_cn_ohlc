ca15e33b22ddec435c66dff4a74b8db0399604b3
aquinas <thisissean2000@gmail.com>
2022-03-04 03:17:10 +0800

    change run_sina_M5.sh to full path

--- run_sina_M5.sh
+++ run_sina_M5.sh
@@ -2,6 +2,6 @@
 
 source /home/sean/code/venv_py3810/bin/activate
 export PYTHONPATH=/home/sean/code/cn_ex_sync
-if [ -z "$STY" ]; then exec screen -dm -S screenName /bin/bash "$0"; fi
+#if [ -z "$STY" ]; then exec screen -dm -S screenName /bin/bash "$0"; fi
 python /home/sean/code/cn_ex_sync/sina/start.py
 
