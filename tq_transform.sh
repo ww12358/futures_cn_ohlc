762395602d99ee33c60035dbd395de66b6b269af
aquinas <thisissean2000@gmail.com>
2022-03-05 00:48:06 +0800

    add tq_transform.sh

---
+++ tq_transform.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash 
+
+source /home/sean/code/venv_py3810/bin/activate
+export PYTHONPATH=/home/sean/code/cn_ex_sync
+#if [ -z "$STY" ]; then exec screen -dm -S screenName /bin/bash "$0"; fi
+python /home/sean/code/cn_ex_sync/sina/tq_transform.py --major
+
