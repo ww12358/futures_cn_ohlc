371f662c2b6da2e36da001094884e34cd6cde97d
aquinas <thisissean2000@gmail.com>
2022-04-28 23:55:01 +0800

    volume 0 problem

--- sina/kMem.py
+++ sina/kMem.py
@@ -169,12 +169,12 @@ class kMem:
         if len(self.dfs) > 0:
             dfs = self.dfs.values()
             df_concat = pd.concat(dfs, axis=0)
-            # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-            #     print(df_concat)
+            with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+                print(df_concat)
 
             # df_concat.loc[df_concat['volume'] == 0, 'volume'] = 1
-            # df_concat = df_concat[df_concat["volume"] > 1]      # avoid divide by 0 when wavg
-            # print(self.symbol, df_concat)
+            df_concat = df_concat[df_concat["volume"] > 0]      # avoid divide by 0 when wavg
+            print(self.symbol, df_concat)
             g = df_concat.groupby(df_concat.index, sort=True)
             # g.apply(print)
             # for date, group in g:
