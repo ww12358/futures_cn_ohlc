ec3f51afb7fd637fd911a925a8f8ce97caa7edae
aquinas <thisissean2000@gmail.com>
2022-05-05 14:28:22 +0800

    beta

--- sina/include.py
+++ sina/include.py
@@ -20,10 +20,10 @@ all_symbols = dce_symbols + cze_symbols + shfe_symbols + ine_symbols + cffex_sym
 # exclude_li = ['LR', 'PM', 'RI', 'JR', 'WH', 'RS', 'PF', 'BB', 'FB', 'RR', 'WR']
 # all_symbols = [e for e in all_symbols if e not in exclude_li]
 com_symbols = dce_symbols + cze_symbols + shfe_symbols + ine_symbols
-watch_list = ["CU", "AL", "ZN", "NI", "RB", "RU",
-              # "BU",
+watch_list = ["CU", "AL", "ZN", "NI", "RB", "RU", "SI",
+              "BU",
               "I", "A", "B", "M", "Y", "P", "JM", "L", "PP", "LH", "C",
-              "TA", "EG", "UR", "SR", "CF", "MA", "FG", "ZC", "OI", "RM",
+              "TA", "EG", "UR", "SR", "CF", "MA", "FG", "ZC", "OI", "RM", "SA",
               "AG", "AU",
               "SC", "FU"]
 
