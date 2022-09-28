beab4f925b19aac309778ba79b1608ca4e0df35f
aquinas <thisissean2000@gmail.com>
2022-09-28 14:44:19 +0800

    untrack script

--- script/transform_sina_M30_all.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-source /home/sean/code/venv_py3800/bin/activate
-export PYTHONPATH=/home/sean/code/cn_ex_sync
-
-python /home/sean/code/cn_ex_sync/sina/sinaM5origin_transform.py --all --freq 30min && \
-python /home/sean/code/cn_ex_sync/vw/mono.py --freq M30 --symbol ALL
